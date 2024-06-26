0ad4cbe5ae5f3e154234051b1a8d7e15cd17921e
Vladislav <84844190+VladBarbashov@users.noreply.github.com>
2024-06-26 10:52:34 +0300

    Delete example/src directory

--- example/src/model/flower.model.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "sequelize";
-
-export interface Flower{
-    id: number;
-    name: string;
-    price: number;
-}
-
-export class FlowerTable extends Model<Flower>{}
--- example/src/model/order.model.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "sequelize";
-
-export interface Order{
-    id: number;
-    name: string;
-    description: string|null;
-}
-
-export class OrderTable extends Model<Order>{}
